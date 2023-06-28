2b4b27c6b2d86ebc0abf0a3348dd57cb920cca1a
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-06-28 13:56:37 +0300

    FIBONACCI ASSIGNMENET First TEST with fibo(1) PASSED after function definition in fibonacci file

---
+++ fibonacci.py
@@ -0,0 +1,4 @@
+# Created the fibo function to use inside my test_fibonacci.py file 
+def fibo(my_number):
+    if my_number == 1:
+        return [0] 
---
+++ test_fibonacci.py
@@ -0,0 +1,14 @@
+# FIBONACCI ASSIGNMENT
+
+from fibonacci import fibo
+
+# my 6 list of fibonacci numbers to use  0,1,1,2,3,5
+
+# The first fibo number can be a 0, but we ignored it
+# def test_fibonacci_0():
+#     assert fibo(0) == []
+ 
+def test_fibonacci_1():
+    assert fibo(1) == [0]
+# THis test FAILED, no fibo function to use
+#  To PASS the test we created a fibo function to use in assert, and imported
